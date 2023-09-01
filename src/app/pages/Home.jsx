61221322a27868664970ecd1047fd78b48caae04
iamshiv007 <iamshiv20032003@gmail.com>
2023-09-01 14:26:11 +0530

    home responsive

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -30,30 +30,31 @@ const Home = () => {
 
   return (
     <Fragment>
-      <div className='min-h-[100vh] flex items-center mx-[200px] justify-between'>
+      <div className='min-h-[100vh] mx-[20px] md:mx-[100px] lg:mx-[200px] pt-[80px] md:pt-0 md:flex items-center justify-between'>
         <div>
-          <p className='text-5xl py-2 font-semibold'>Hi,</p>
-          <p className='text-5xl py-2 font-semibold'>I&apos;m Shivraj</p>
-          <p className='text-5xl py-2 font-semibold'>
+          <p className='text-4xl sm:text-5xl py-2 font-semibold'>Hi,</p>
+          <p className='text-4xl sm:text-5xl py-2 font-semibold'>
+            I&apos;m Shivraj
+          </p>
+          <p className='text-4xl sm:text-5xl py-2 font-semibold'>
             a web <span className='text-[#3498db]'> Developer</span>
           </p>
         </div>
 
         <div
-          className='bg-[url(http://localhost:3000/images/profile.png)] w-[300px] h-[300px] bg-cover rounded-full hover:bg-gray-500 hover:cursor-pointer bg-blend-multiply'
+          className='bg-[url(/images/profile.png)] w-[300px] h-[300px] bg-cover rounded-full bg-gray-500 hover:cursor-pointer hover:bg-blend-multiply m-auto md:m-0 mt-[40px] md:mt-0'
           ref={profileRef}
         >
           {showResume ? (
             <Link
               className='w-full h-full flex justify-center items-center underline text-xl'
               href='https://drive.google.com/file/d/1AWiU8oWJi5rY0nl6d3iMHOjevBq7izOU/view?usp=drive_link'
+              passHref
               target='_blank'
             >
               Resume <FiExternalLink />
             </Link>
-          ) : (
-            ""
-          )}
+          ) : null}
         </div>
       </div>
     </Fragment>
