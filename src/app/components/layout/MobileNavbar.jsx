27451948cecc657d430aa3146bd565c439f2f8e7
iamshiv007 <iamshiv20032003@gmail.com>
2023-09-02 22:04:49 +0530

    scroll and navbar fix

--- src/app/components/layout/MobileNavbar.jsx
+++ src/app/components/layout/MobileNavbar.jsx
@@ -1,68 +1,16 @@
 "use client";
-import React, { Fragment, useContext, useState, useEffect } from "react";
+import React, { Fragment, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { DiTechcrunch } from "react-icons/di";
-import { BsFillLightningChargeFill } from "react-icons/bs";
-import { TbBulbFilled } from "react-icons/tb";
 import { IoMdClose } from "react-icons/io";
 
-import { ThemeContext } from "../../context/ThemeContext";
 import { NavbarData } from "../../portfolioData/NavbarData";
 
 const MobileNavbar = () => {
   const [showMenu, setShowMenu] = useState(false);
-  const { theme, setTheme } = useContext(ThemeContext);
-
-  useEffect(() => {
-    let prevScrollPos = window.scrollY;
-    const navbar = document.querySelector(".mobileNavbar");
-
-    window.onscroll = function () {
-      const currentScrollPos = window.scrollY;
-
-      if (prevScrollPos > currentScrollPos) {
-        navbar.style.top = "0";
-      } else {
-        navbar.style.top = "-50px"; // Hides the navbar
-      }
-
-      prevScrollPos = currentScrollPos;
-    };
-  }, []);
 
   return (
     <Fragment>
-      {/* Header */}
-      <div className='md:hidden mobileNavbar transition-all flex justify-between fixed top-0 w-full py-3 px-5 shadow-gray-300 dark:shadow-gray-800 shadow-md z-10 bg-[#ffffffcc] dark:bg-[#000000cc] backdrop-filter backdrop-blur-lg'>
-        <div className='flex gap-4 items-center'>
-          <button
-            className='text-3xl font-semibold'
-            onClick={() => setShowMenu(!showMenu)}
-          >
-            <GiHamburgerMenu />
-          </button>
-
-          <p className='flex text-gray-400'>
-            <span className='font-bold text-lg'>SHIV</span> <DiTechcrunch />
-          </p>
-        </div>
-
-        <div>
-          <button
-            className='text-2xl font-semibold text-[#159e6e] dark:text-[#17c1ff]'
-            onClick={() => setTheme(theme === "dark" ? null : "dark")}
-          >
-            {theme === "dark" ? (
-              <TbBulbFilled />
-            ) : (
-              <BsFillLightningChargeFill />
-            )}
-          </button>
-        </div>
-      </div>
-      
       {/* Sidebar */}
       <div
         className={`fixed top-0 left-0 w-full h-screen z-10 bg-[rgba(255,255,255,0.3)] dark:bg-[rgba(0,0,0,0.3)] ${
--- src/app/components/layout/Navbar.jsx
+++ src/app/components/layout/Navbar.jsx
@@ -1,36 +1,40 @@
 "use client";
-import React, { Fragment, useContext, useEffect } from "react";
+import React, { Fragment, useContext, useEffect, useState } from "react";
 import Link from "next/link";
 import { DiTechcrunch } from "react-icons/di";
 import { BsFillLightningChargeFill } from "react-icons/bs";
 import { TbBulbFilled } from "react-icons/tb";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 import { ThemeContext } from "../../context/ThemeContext";
 import { NavbarData } from "../../portfolioData/NavbarData";
 
 const Navbar = () => {
   const { theme, setTheme } = useContext(ThemeContext);
+  const [top, setTop] = useState("0");
 
   useEffect(() => {
     let prevScrollPos = window.scrollY;
-    const navbar = document.querySelector(".navbar");
 
     window.onscroll = function () {
       const currentScrollPos = window.scrollY;
 
       if (prevScrollPos > currentScrollPos) {
-        navbar.style.top = "0";
+        setTop("0");
       } else {
-        navbar.style.top = "-50px"; // Hides the navbar
+        setTop("-50px"); // Hides the navbar
       }
 
       prevScrollPos = currentScrollPos;
     };
-  }, []);
+  });
 
   return (
     <Fragment>
-      <div className='navbar transition-all fixed top-0 hidden md:flex gap-4 justify-between w-full p-3 shadow-gray-300 dark:shadow-gray-800 shadow-md z-10 px-8 bg-[#ffffffcc] dark:bg-[#000000cc] backdrop-filter backdrop-blur-lg'>
+      <div
+        className='navbar transition-all fixed hidden md:flex gap-4 justify-between w-full p-3 shadow-gray-300 dark:shadow-gray-800 shadow-md z-10 px-8 bg-[#ffffffcc] dark:bg-[#000000cc] backdrop-filter backdrop-blur-lg'
+        style={{ top: top }}
+      >
         <p className='flex text-gray-400'>
           <span className='font-bold text-lg'>SHIV</span> <DiTechcrunch />
         </p>
@@ -53,6 +57,38 @@ const Navbar = () => {
         >
           {theme === "dark" ? <TbBulbFilled /> : <BsFillLightningChargeFill />}
         </button>
+      </div>
+
+      {/* mobile Header */}
+      <div
+        className='md:hidden navbar transition-all flex justify-between fixed w-full py-3 px-5 shadow-gray-300 dark:shadow-gray-800 shadow-md z-10 bg-[#ffffffcc] dark:bg-[#000000cc] backdrop-filter backdrop-blur-lg'
+        style={{ top }}
+      >
+        <div className='flex gap-4 items-center'>
+          <button
+            className='text-3xl font-semibold'
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            <GiHamburgerMenu />
+          </button>
+
+          <p className='flex text-gray-400'>
+            <span className='font-bold text-lg'>SHIV</span> <DiTechcrunch />
+          </p>
+        </div>
+
+        <div>
+          <button
+            className='text-2xl font-semibold text-[#159e6e] dark:text-[#17c1ff]'
+            onClick={() => setTheme(theme === "dark" ? null : "dark")}
+          >
+            {theme === "dark" ? (
+              <TbBulbFilled />
+            ) : (
+              <BsFillLightningChargeFill />
+            )}
+          </button>
+        </div>
       </div>
     </Fragment>
   );
