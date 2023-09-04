487e77b2ff7330885fcf877940da5ed5cdc1b3d4
iamshiv007 <iamshiv20032003@gmail.com>
2023-09-04 12:08:58 +0530

    animation: SocialMedia

--- src/app/components/others/socialMedia/SocialMedia.jsx
+++ src/app/components/others/socialMedia/SocialMedia.jsx
@@ -1,15 +1,40 @@
-import React, { Fragment } from "react";
+"use client";
+import React, { Fragment, useState, useRef, useEffect } from "react";
 import Link from "next/link";
 
 import { SocialMediaData } from "../../../portfolioData/socialMedia/SocialMediaData";
 
 const SocialMedia = () => {
+  const [isSocial, setIsSocial] = useState(false);
+  const socialRef = useRef();
+
+  useEffect(() => {
+    const socialObserver = new IntersectionObserver(
+      ([socialEntry]) => {
+        setIsSocial(socialEntry.isIntersecting);
+      },
+      {
+        rootMargin: "-60px",
+      }
+    );
+
+    socialObserver.observe(socialRef.current);
+
+    if (isSocial) {
+      socialRef.current.classList.add("social-show");
+    } else {
+      socialRef.current.classList.remove("social-show");
+    }
+  }, [isSocial]);
+
   return (
     <Fragment>
-      <div className='absolute left-10 bottom-3'>
+      <div className='absolute left-10 bottom-3 social-hide' ref={socialRef}>
         {SocialMediaData.map((social) => (
           <div
-            className={"text-xl text-white rounded-full w-fit p-2 mb-3"}
+            className={
+              "text-xl text-white rounded-full p-2 mb-3 hover:scale-[1.2] transition-all duration-500"
+            }
             key={social.name}
             style={{ background: social.color }}
           >
