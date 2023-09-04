5677e65267f9fd842bd34f405282d005e5f87319
iamshiv007 <iamshiv20032003@gmail.com>
2023-09-04 13:20:16 +0530

    animation: About

--- src/app/pages/about/About.jsx
+++ src/app/pages/about/About.jsx
@@ -1,13 +1,47 @@
+"use client";
+import React, { Fragment, useState, useRef, useEffect } from "react";
 import Image from "next/image";
-import React, { Fragment } from "react";
 import { FaUserCheck } from "react-icons/fa";
 
 const About = () => {
+  const [isAbout, setIsAbout] = useState(false);
+
+  const aboutRef = useRef();
+  const profile2Ref = useRef();
+  const aboutInfoRef = useRef();
+
+  useEffect(() => {
+    const getScreenWidth = () =>
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
+
+    const aboutObserver = new IntersectionObserver(
+      ([aboutEntry]) => {
+        setIsAbout(aboutEntry.isIntersecting);
+      },
+      {
+        rootMargin: `${getScreenWidth() <= 700 ? "-100px" : "-300px"}`,
+      }
+    );
+
+    aboutObserver.observe(aboutRef.current);
+
+    if (isAbout) {
+      profile2Ref.current.classList.add("slide-in");
+      aboutInfoRef.current.classList.add("slide-in");
+    } else {
+      profile2Ref.current.classList.remove("slide-in");
+      aboutInfoRef.current.classList.remove("slide-in");
+    }
+  }, [isAbout]);
+
   return (
     <Fragment>
       <section
-        className='shadow-zinc-300 dark:shadow-zinc-700 shadow-sm'
+        className='shadow-zinc-300 dark:shadow-zinc-700 shadow-sm overflow-x-hidden'
         id='about'
+        ref={aboutRef}
       >
         <h2 className='text-3xl font-bold text-center p-4 flex justify-center gap-3'>
           <FaUserCheck /> About me
@@ -16,14 +50,18 @@ const About = () => {
           <div>
             <Image
               alt='photo'
-              className='rounded shadow-sm shadow-gray-400 dark:shadow-gray-700 bg-gray-400 m-auto'
+              className='rounded translate-x-[-550px] opacity-0 transition-all duration-700 shadow-sm shadow-gray-400 dark:shadow-gray-700 bg-gray-400 m-auto'
               height={460}
+              ref={profile2Ref}
               src='/images/profile2.jpg'
               width={260}
             />
           </div>
 
-          <div className='text-lg mt-4 md:mt-0 md:w-[50%] text-center md:text-left'>
+          <div
+            className='text-lg translate-x-[900px] opacity-0 transition-all duration-700 mt-4 md:mt-0 md:w-[50%] text-center md:text-left'
+            ref={aboutInfoRef}
+          >
             <p>
               My name is{" "}
               <span className='text-[#159e6e] dark:text-[#17c1ff] font-semibold'>
