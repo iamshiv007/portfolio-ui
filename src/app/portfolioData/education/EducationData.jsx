3fabec022ab28d41b3ac20479cb8d1944816978e
iamshiv007 <iamshiv20032003@gmail.com>
2023-09-05 08:13:12 +0530

    update: Education page data, from EducationData

---
+++ src/app/portfolioData/education/EducationData.jsx
@@ -0,0 +1,26 @@
+export const EducationData = [
+  {
+    name: "Bachelor of Science",
+    image:
+      "https://shiv-s-portfolio.vercel.app/_next/image?url=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcQ3xSTnbK57kUx2XgDd_zzXJp5uXPSNUmeZ9g%26usqp%3DCAUF&w=384&q=75",
+    schoolOrCollege: "GOVERNMENT COLLEGE, KOTA",
+    fromTo: "2021 - 2022",
+    statusOrPrecentage: "Dropped in second year",
+  },
+  {
+    name: "Senior Secondary Examination",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ5r_rqg74FNoa-77Q9_bEqu3AyYqK8ngvTA&usqp=CAU",
+    schoolOrCollege: "SWAMI TEONRAM ALOK SR. SEC. SCHOOL DADABARI, KOTA",
+    fromTo: "2021",
+    statusOrPrecentage: "96.80 %",
+  },
+  {
+    name: "Secondary Examination",
+    image:
+      "https://shiv-s-portfolio.vercel.app/_next/image?url=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcQpCDvRmnjiBTgBd9WyPHlJHEPzRz2shaS3YQ%26usqp%3DCAU&w=384&q=75",
+    schoolOrCollege: "A.V.M. SECONDARY SCHOOL, RAWATBHATA",
+    fromTo: "2019",
+    statusOrPrecentage: "93.33%",
+  },
+];
